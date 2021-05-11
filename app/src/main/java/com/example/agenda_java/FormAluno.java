42c2aa74de4cccc6ee9ed22f31e1e3ad887f6b4e
lucianolopez85 <lopezz.lucio85@gmail.com>
2021-05-11 15:41:14 -0300

    first commit

---
+++ app/src/main/java/com/example/agenda_java/FormAluno.java
@@ -0,0 +1,58 @@
+package com.example.agenda_java;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FormAluno extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Novo aluno";
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
+    private final AlunoDAO dao = new AlunoDAO();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_aluno);
+        setTitle(TITULO_APPBAR);
+        inicializacaoDosCampos();
+        configuraBotaoSalvar();
+    }
+
+    private void configuraBotaoSalvar() {
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Aluno alunoCriado = criaAluno();
+                salva(alunoCriado);
+            }
+        });
+    }
+
+    @NonNull
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        return new Aluno(nome, telefone, email);
+    }
+
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+    private void salva(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
+}
